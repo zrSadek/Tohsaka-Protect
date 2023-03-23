69d6f8157a8e57c6c2388226602e9415b09c7654
zrSadek <114314659+zrSadek@users.noreply.github.com>
2023-03-23 14:26:52 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: 'LE TOKEN DE TON BOT',
+    prefix: '+',
+    owners: ['TON ID'],
+    database: {
+        name: 'LE NOM DE TA BASE DE DONNEE',
+        password: 'LE MOT DE PASSE DE TA BASE DE DONNEE'
+    }
+}
